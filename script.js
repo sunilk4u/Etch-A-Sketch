8c36a78b08e0cea3e02f0571bef7a131712e6bd2
Sunil Kumar <sunilk10099@gmail.com>
2022-05-18 18:11:08 +0530

    Add custom canvas size option

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function createGrid() {
+function createGrid(size) {
   const container = document.querySelector(".container");
 
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < size; i++) {
     const gridLine = document.createElement("div");
     gridLine.classList.add("gridLine");
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
       const singleGrid = document.createElement("div");
       singleGrid.classList.add("singleGrid");
       gridLine.appendChild(singleGrid);
@@ -14,4 +14,20 @@ function createGrid() {
   }
 }
 
-createGrid();
+createGrid(16);
+
+function askInput() {
+  let newSize = Number(
+    prompt("Enter new size for canvas (must be smaller or equal to 100)")
+  );
+  if (Number.isInteger(newSize) && newSize <= 100) {
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+    createGrid(newSize);
+  } else {
+    alert("Wrong Input! Enter Number Value Again");
+  }
+}
+
+const setNew = document.querySelector(".custom");
+setNew.addEventListener("click", askInput);
